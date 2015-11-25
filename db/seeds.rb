8a0154782c89812e89e215f6f87617d985f25a52
truonglocbinh <truonglocbinh93@gmail.com>
2015-11-25 16:07:16 +0700

    fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,33 @@
-user = User.find(1)
-entries = user.entries.take(6)
-50.times do
-  content = Faker::Lorem.sentence(5)
-  entries.each { |entry| entry.comments.create!(content: content, user_id: user.id) }
+# Users
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             )
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name: name,
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              )
 end
+
+# Entry
+users = User.order(:created_at).take(6)
+50.times do
+  title = Faker::Lorem.sentence(2)
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.entries.create!(title: title,content: content) }
+end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
