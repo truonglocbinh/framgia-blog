dc0aedd85b055613fffbe6edf752fd49bb525b21
truonglocbinh <truonglocbinh93@gmail.com>
2015-11-25 15:32:12 +0700

    change comment function with ajax form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,9 @@
 class CommentsController < ApplicationController
   def create
   	@comment = Comment.new
-  	@comment.content = params[:value]
+  	@comment.content = params[:comment][:content]
   	@comment.user_id = current_user.id
-  	@comment.entry_id = params[:entry_id].to_i
+  	@comment.entry_id = params[:comment][:entry_id]
   	if @comment.save
       @comments = Entry.find( @comment.entry_id ).comments.limit(3)
   	  respond_to do |format| 
